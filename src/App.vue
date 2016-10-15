04af8a4d50a08ba4a5f9a7dfeae840507f3b6b73
Michael Lobel <mglobel@gmail.com>
2016-10-15 13:31:24 -0500

    add featured tracks and shows sections

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,31 @@
       </div>
       <p>Download EP or listen to on Soundcloud.</p>
     </section>
+    
+    <hr />
+    <section class="latest-tracks">
+      <h3>Latest Tracks:</h3>
+      <div class="soundcloud-widget">
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/254863765&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+      </div>
+      <div class="soundcloud-widget">
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/250838184&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+      </div>
+    </section>
+
+    <hr />
+    <section class="shows">
+      <h3>Shows</h3>
+
+      <h4>Upcoming:</h4>
+      <ul>
+      </ul>
+
+      <h4>Past:</h4>
+      <ul>
+
+      </ul>
+    </section>
 
     <hr />
     <footer>
