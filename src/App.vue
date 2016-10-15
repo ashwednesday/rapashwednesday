2c1a21ad263cf2217f4e69c73281c242cd1a15b9
Michael Lobel <mglobel@gmail.com>
2016-10-15 00:33:47 -0500

    basic responsive run through

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,9 @@
           </div>
           <button>Download EP</button>
         </div>
-        <iframe width="50%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/153720446&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+        <div class="soundcloud-widget">
+          <iframe scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/153720446&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+        </div>
       </div>
       <p>Download EP or listen to on Soundcloud.</p>
     </section>
