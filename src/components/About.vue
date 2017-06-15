eb21ba4a39d87b92009d998f9a7b5d693e43a96a
Michael Lobel <mglobel@gmail.com>
2017-06-14 21:20:30 -0500

    update about fixes #20

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,16 +6,13 @@
     </header>
     <section>
       <p>
-        Ash Wednesday & The Heroic Dose are best known for their obscure sample game, rich melodies, and most of all, Ash Wednesday’s fuck-you-nicely lyricism. Her verbose rhymes take on themes of power, social control, and self-rule, all while keeping the floor jumping. The Heroic Dose [Mike Balish on keyboard & Joel Handley on drums] lend their musical sensibilities.
+        Ash Wednesday & The Heroic Dose are best known for their obscure sample game, rich melodies, and Ash’s fuck-you-nicely lyricism. Her rhymes explore themes of power, social control, and self-rule while keeping the floor jumping. The Heroic Dose [Mike Balish on keyboard & Joel Handley on drums] lend their musical sensibilities.
       </p>
       <p>
-        Though Ash has been talking shit and taking names since day one, she’s been doing it with a band behind her since 2012. Ash got into rapping in 2010 after a long fascination with the art form, which she discovered around age 12. Her first appearances on stage in Chicago were at Subterranean Lounge’s 606 Freestyle Open Mic where she earned her moniker Ash Wednesday.
+        Since 2012 Ash Wednesday & The Heroic Dose have performed at venues around Chicago including Tonic Room, Township, Cole's, Hostel Earphoria DIY, @north Bar, and Quenchers. They’ve played alongside acts like SKECH185, Lamon Manuel, Jyroscope, Rich Jones, and Pivot Gang members MFN Melo, Squeak PIVOT, and dinnerwithjohn (#ForeverJohnWalt).
       </p>
       <p>
-        During a show with her band at local venue Moe’s Tavern in 2014, Mike Balish (a childhood friend of Joel Handley’s), approached the band after their performance and in a priceless display of no-fucks-given, asked “where’s the fucking melody?” Those balls landed him a spot on the team and the Heroic Dose was born.
-      </p>
-      <p>
-        Ash Wednesday & The Heroic Dose have played venues across Chicago including Tonic Room, Township, @North Bar, and Quenchers. They’ve played alongside acts like Tomorrow Kings, Rich Jones, and Pivot Gang members MFN Melo, Squeak PIVOT, and dinnerwithjohn. In 2015 they released a project called Ugly on Purpose. 
+        In 2015 Ash Wednesday & The Heroic Dose released a project called Ugly on Purpose. A new project is underway and will be released in 2017.
       </p>
     </section>
   </article>
